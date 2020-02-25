4540efd854c976c4707f8edf70d150987d282d09
Leandro de Abreu Sucupira <leandro.abreu.1289@gmail.com>
2020-02-25 12:05:50 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/leandro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/leandro/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.leandro.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leandro.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionou";
+	public List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(1,"Informatica");
+		Categoria categoria2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 }
